3bc179fa35ff6157b16dd77da393a80942fd596d
gitu <gitu@lgq>
2024-08-01 09:18:10 +0300

    init

---
+++ calc.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+def main():
+    if len(sys.argv) < 2:
+        print("Usage: calc.py <expression>")
+        return
+
+    expression = ''.join(sys.argv[1:]).replace(" ", "")
+
+    print("Unsupported operator.")
+    return
+
+
+if __name__ == "__main__":
+    main()
