61de652ae6249e1389b39a4e607617ef4ea5db34
gitu-dark <gitu@lgq>
2024-08-01 10:16:44 +0300

    div

--- calc.py
+++ calc.py
@@ -18,8 +18,14 @@ def main():
     elif '*' in expression:
         num1, num2 = expression.split('*')
         result = float(num1) * float(num2)
+    elif '/' in expression:
+        num1, num2 = expression.split('/')
+        if float(num2) == 0:
+            print("Error: Division by zero.")
+            return
+        result = float(num1) / float(num2)
     else:
-        print("Unsupported operator. Use +, -, *")
+        print("Unsupported operator. Use +, -, *, or /.")
         return
 
     print(result)
